1a651cd01bcc11e48f4a1546e617134bf2fd0382
DFAle <aale0800z@gmail.com>
2025-04-17 01:17:00 -0300

    agregando ejercicio1 y ejercicio4

---
+++ JS/ejercicio1.js
@@ -0,0 +1,7 @@
+let integrantes = 'David Marcelo, Mayra Zalazar, Candela Moreno, Alejandro Cuellar'
+
+const mostarMensaje = ( ) =>{   
+    console.log(`Somos ${integrantes} y estamos aprendiendo JavaScript`)
+}
+
+mostarMensaje()
