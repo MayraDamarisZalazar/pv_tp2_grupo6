a7b8d39d98d763f5a983d7726c15cbb7c6eae94c
candelampv <candelanicole145gmail.com>
2025-04-18 03:39:45 -0300

    agregado ejercicio 16

---
+++ JS/ejercicio16.js
@@ -0,0 +1,6 @@
+function sumarRango(inicial, final) {let suma = 0;
+for (let i = inicial; i <= final; i++) {suma +=i;} return suma;
+}
+//ejemplos:
+console.log(sumarRango(1,5)); 
+console.log(sumarRango(3,8));
