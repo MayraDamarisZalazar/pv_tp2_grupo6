50678bc98e0ca5278f8cf3cad9340690d3bd7541
MayraDamarisZalazar <42953865@fi.unju.edu.ar>
2025-05-23 16:39:20 -0300

    arreglando  ejercicio 2

--- JS/ejercicio2.js
+++ JS/ejercicio2.js
@@ -1,4 +1,7 @@
-for (let i = 1; i <= 10; i++) {
-    let numeroPar = i * 2;
-    console.log(numeroPar);
-}
+const mostrarPares = () => {
+  for (let i = 1; i <= 10; i++) {
+    console.log(i * 2);
+  }
+};
+
+mostrarPares();
