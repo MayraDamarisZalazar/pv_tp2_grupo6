b7022cd9bebd9c6daf2fc716ffbaf5e9472d7844
DFAle <aale0800z@gmail.com>
2025-05-23 22:10:54 -0300

    11

--- JS/ejercicio11.js
+++
@@ -1,7 +0,0 @@
-let kilometros = 200;
-let combustible = 10; 
-let consumo = combustible/kilometros; 
-
-console.log("kilometros recorridos: ", kilometros);
-console.log("litros de conbustible consumido: ", combustible);
-console.log("El combustible consumido por kilometro es de: ", consumo)
